de659b775d118950b12e3da8ee05d2a30d7bc313
Emilia <167714837+girlnox@users.noreply.github.com>
2026-04-04 01:36:44 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -15,18 +15,27 @@ func main() {
 
 	if len(os.Args) > 1 {
 
-		if os.Args[1] == "get" {
+		if os.Args[1] == "receive" {
 			get_local_ip(os.Args[2])
 			if len(os.Args) > 2 {
 				receiver_setup(os.Args[2])
 			} else {
-				fmt.Println("Please specify a port number")
+				fmt.Println("Please specify a port number.")
+			}
+		}
+
+		if os.Args[1] == "serve" {
+			if len(os.Args) > 2 {
+				get_local_ip(os.Args[2])
+				server_setup(os.Args[2], os.Args[3])
+			} else {
+				fmt.Println("Please specify a port number.")
 			}
 		}
 
 		if os.Args[1] == "give" {
 			if len(os.Args) > 3 { // abomination incoming
-				fmt.Println("Giving file")
+				fmt.Println("Giving file...")
 				out, err := exec.Command("curl", "-X", "POST", "-F", "file=@"+os.Args[2], "http://"+os.Args[3]+"/upload").CombinedOutput()
 				if err != nil {
 					log.Fatal(err)
@@ -35,6 +44,19 @@ func main() {
 			} else {
 				fmt.Println("Specify a file to send and the target device's address in the format: ip_address:port")
 			}
+		}
+
+		if os.Args[1] == "get" {
+			if len(os.Args) > 3 { // abomination incoming
+				fmt.Println("Getting file...")
+				out, err := exec.Command("curl", "-O", "http://"+os.Args[3]+"/"+os.Args[2]).CombinedOutput()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf(string(out))
+				} else {
+					fmt.Println("Specify a file to get and the server's address")
+				}
 		}
 
 		if os.Args[1] == "pack" {
@@ -109,7 +131,7 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) { // i just vibe 
 	fmt.Fprintf(w, "Successfully uploaded file: %s", handler.Filename)
 }
 
-func receiver_setup(server_port string) {
+func receiver_setup(server_port string) { // #isuckatcodingsoiuseinternetcodelikealoser
 	http.HandleFunc("/upload", uploadFileHandler)
 	fmt.Println("Server set up, ready to transmit!")
 
@@ -124,6 +146,27 @@ func receiver_setup(server_port string) {
 	var input string
 	fmt.Scanln(&input) // wait until user presses thingie
 	fmt.Println("Stopping the server...")
+}
+
+func server_setup(server_port string, server_directory string) {
+	// do the file serving thing from the chosen directory
+	fileServer := http.FileServer(http.Dir("./" + server_directory))
+
+	// remove annoying extra handle need
+	http.Handle("/", fileServer)
+
+	// Start the server on chosen portaaaaaa yayayay
+	go func() {
+
+		if err := http.ListenAndServe(":"+server_port, nil); err != nil {
+			log.Fatalf("LitenAndServer: %s", err)
+		}
+	}()
+	fmt.Println("Server directory: " + server_directory)
+	fmt.Println("Press Ender to stop the server...")
+	var input string
+	fmt.Scanln(&input) // wait for user presses enter grrr muhaha
+	fmt.Println("Stopping the server")
 }
 
 func get_local_ip(server_port string) {
@@ -153,12 +196,19 @@ func get_local_ip(server_port string) {
 func help_message() {
 	fmt.Println()
 	fmt.Println("rawr - Simple package manager")
-	fmt.Println("Usage info: \n")
-	fmt.Println("rawr get [port]: host an upload server for receiving packages")
-	fmt.Println("rawr give [file] [ip_address:port]: give a package to an open host")
-	fmt.Println("rawr install [package]: install a package")
+	fmt.Println("Usage info:")
+	fmt.Println()
 	fmt.Println("rawr pack [folder]: create a rawr package")
 	fmt.Println("rawr unpack [package]: unpackage a packaged rawr package")
-	fmt.Println("rawr help/--help/(nothing): Display this message")
+	fmt.Println()
+	fmt.Println("rawr receive [port]: host an upload server for receiving packages")
+	fmt.Println("rawr give [file] [ip:port]: give a package to an open receiver")
+	fmt.Println()
+	fmt.Println("rawr serve [port]: host a download server for serving packages")
+	fmt.Println("rawr get [ip:port/saved name] [package path]")
+	fmt.Println()
+	fmt.Println("rawr install [package]: install a package")
+	fmt.Println()
+	fmt.Println("rawr help/--help/nothing: Display this message")
 	fmt.Println("")
 }
